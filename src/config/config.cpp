a485c46624bf19b0c291d619d0d9db927e16ec4b
br00cew4yne <nncnep@gmail.com>
2025-01-07 09:01:03 +0100

    feat: better values for the obstacles

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -9,12 +9,12 @@ unsigned int Config::windowWidth = 1440;
 unsigned int Config::windowHeight = 1080;
 
 // Obstacles.
-unsigned int Config::obstacleAmount = 3;
-float Config::obstacleWidth = 0.1;
-float Config::obstacleHeight = 0.5;
-float Config::obstacleDepth = 1.0;
-float Config::obstacleStep = -0.1;
-float Config::obstacleOffset = 5.0;
+float Config::obstacleWidth = 0.1f;
+float Config::obstacleHeight = 0.5f;
+float Config::obstacleDepth = 1.0f;
+float Config::obstacleStep = -0.1f;
+float Config::obstacleOffset = 5.0f;
+unsigned int Config::obstacleAmount = 8;
 
 // Animations.
 float Config::animationSpeed = 1.5f;
