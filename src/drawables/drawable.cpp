4a78f76569f5d6c7d326b5cb9414b77c38b3ebaf
br00cew4yne <nncnep@gmail.com>
2025-01-09 14:40:00 +0100

    feat: pixel look using GL_NEAREST for magnification

--- src/drawables/drawable.cpp
+++ src/drawables/drawable.cpp
@@ -107,7 +107,7 @@ GLuint Drawable::loadTexture(std::string path) {
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glGenerateMipmap(GL_TEXTURE_2D);
 
     // Retrieve maximum supported anisotropy level, and set it.
