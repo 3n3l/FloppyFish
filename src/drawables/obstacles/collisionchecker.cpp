5b96ff2983ad2dbcd1efafd391914f8a59c1ae66
QuiKong <73022524+QuiKong@users.noreply.github.com>
2025-01-16 21:17:30 +0100

    Update collisionchecker.cpp

--- src/drawables/obstacles/collisionchecker.cpp
+++ src/drawables/obstacles/collisionchecker.cpp
@@ -1,16 +1,12 @@
 #include "collisionchecker.h"
 
 bool CollisionChecker::checkCollision(const FloppyFish& fish, const Obstacle& obstacle) {
-    // Get fish bounding box
-    float fishX, fishY, fishWidth, fishHeight;
-    fish.getBounds(fishX, fishY, fishWidth, fishHeight);
-
     // Get obstacle bounding box (UpperPart und LowerPart)
     float obsX, obsY, obsWidth, obsHeight;
     obstacle.getBounds(obsX, obsY, obsWidth, obsHeight);
 
     // Check if the rectangles overlap (AABB - Axis-Aligned Bounding Box Kollision)
-    if (fishX + fishWidth > obsX && fishX < obsX + obsWidth && fishY + fishHeight > obsY && fishY < obsY + obsHeight) {
+    if (fish.x() + fish.width() > obsX && fish.x() < obsX + obsWidth && fish.y() + fish.height() > obsY && fish.y() < obsY + obsHeight) {
         return true;  // Collision detected
     }
 
