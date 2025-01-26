dfa0e8039c7c91cd5d1af00e8f1695fb242d4331
br00cew4yne <nncnep@gmail.com>
2025-01-26 19:30:40 +0100

    feat: smaller angles for the flopping

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -37,5 +37,5 @@ float Config::skyRotation = 1.0f / 2400.0f;
 const float Config::velocityBound = -9.8f / 1000.0f;
 const float Config::verticalAcceleration = -0.001f;
 const float Config::verticalVelocity = 0.02f;
-const float Config::lowerAngle = -70.0f;
-const float Config::upperAngle = 35.0f;
+const float Config::lowerAngle = -15.0f;
+const float Config::upperAngle = 15.0f;
