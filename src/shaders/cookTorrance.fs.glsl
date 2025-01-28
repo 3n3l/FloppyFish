aef32a6ff3cae270255952c858e7b53fbd1d69a1
br00cew4yne <nncnep@gmail.com>
2025-01-28 15:04:01 +0100

    fix: initialize fColour in main() to comply with GLSL standard

--- src/shaders/cookTorrance.fs.glsl
+++ src/shaders/cookTorrance.fs.glsl
@@ -31,7 +31,7 @@ const vec3 materialSpecularColour = vec3(1.0f, 1.0f, 1.0f);
 const float pi = 3.14159265358979323846f;
 
 // Send colour to screen.
-layout (location = 0) out vec4 fColour = vec4(0.0f);
+layout (location = 0) out vec4 fColour;
 
 vec3 cook_torrance(vec3 materialDiffuseColour,
                    vec3 materialSpecularColour,
@@ -107,6 +107,7 @@ void main(void)
     // Viewing direction, aka. Omega_out.
     vec3 view_vec = normalize(vView);
 
+    fColour = vec4(0.0f);
     for (int i = 0; i < NUM_LIGHTS; i++) {
         // Direction towards the light, aka. -Omega_in.
         vec3 light_vec = normalize(vLightDir[i]);
